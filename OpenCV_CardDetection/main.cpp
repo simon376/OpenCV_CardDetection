1d036ab14a4c1054876b167bf537867c33e0de0a
simon376 <simon.mueller@st.oth-regensburg.de>
2020-01-02 20:56:23 +0100

    added files to gitignore
    integrated Signal-to-Noise-Ratio and Structural Similarity Index calculation from tutorial to compare cards

--- OpenCV_CardDetection/main.cpp
+++ OpenCV_CardDetection/main.cpp
@@ -3,6 +3,7 @@
 
 */
 #include <iostream>
+#include <iomanip>      // std::setiosflags
 #include <chrono>
 
 #include "opencv2/core/core.hpp"
@@ -50,6 +51,66 @@ void sort_corners(vector <Point2f> & corners);
 Mat flatten(vector <Point2f>& corners);
 bool is_color(const Mat& bgr_image, Scalar low, Scalar high, Scalar low2, Scalar high2);
 
+/*
+Both taken from official OpenCV docs tutorial:
+https://docs.opencv.org/master/d5/dc4/tutorial_video_input_psnr_ssim.html
+
+*/
+double getPSNR(const Mat& I1, const Mat& I2)
+{
+	Mat s1;
+	absdiff(I1, I2, s1);       // |I1 - I2|
+	s1.convertTo(s1, CV_32F);  // cannot make a square on 8 bits
+	s1 = s1.mul(s1);           // |I1 - I2|^2
+	Scalar s = sum(s1);        // sum elements per channel
+	double sse = s.val[0] + s.val[1] + s.val[2]; // sum channels
+	if (sse <= 1e-10) // for small values return zero
+		return 0;
+	else
+	{
+		double mse = sse / (double)(I1.channels() * I1.total());
+		double psnr = 10.0 * log10((255 * 255) / mse);
+		return psnr;
+	}
+}
+
+Scalar getMSSIM(const Mat& i1, const Mat& i2)
+{
+	const double C1 = 6.5025, C2 = 58.5225;
+	/***************************** INITS **********************************/
+	int d = CV_32F;
+	Mat I1, I2;
+	i1.convertTo(I1, d);            // cannot calculate on one byte large values
+	i2.convertTo(I2, d);
+	Mat I2_2 = I2.mul(I2);        // I2^2
+	Mat I1_2 = I1.mul(I1);        // I1^2
+	Mat I1_I2 = I1.mul(I2);        // I1 * I2
+	/*************************** END INITS **********************************/
+	Mat mu1, mu2;                   // PRELIMINARY COMPUTING
+	GaussianBlur(I1, mu1, Size(11, 11), 1.5);
+	GaussianBlur(I2, mu2, Size(11, 11), 1.5);
+	Mat mu1_2 = mu1.mul(mu1);
+	Mat mu2_2 = mu2.mul(mu2);
+	Mat mu1_mu2 = mu1.mul(mu2);
+	Mat sigma1_2, sigma2_2, sigma12;
+	GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
+	sigma1_2 -= mu1_2;
+	GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
+	sigma2_2 -= mu2_2;
+	GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
+	sigma12 -= mu1_mu2;
+	Mat t1, t2, t3;
+	t1 = 2 * mu1_mu2 + C1;
+	t2 = 2 * sigma12 + C2;
+	t3 = t1.mul(t2);                 // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+	t1 = mu1_2 + mu2_2 + C1;
+	t2 = sigma1_2 + sigma2_2 + C2;
+	t1 = t1.mul(t2);                 // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+	Mat ssim_map;
+	divide(t3, t1, ssim_map);        // ssim_map =  t3./t1;
+	Scalar mssim = mean(ssim_map);   // mssim = average of ssim map
+	return mssim;
+}
 
 /* saves all images of currently detected cards into the /images/ folder for use as a reference to compare to*/
 void save_cards()
@@ -85,10 +146,13 @@ void save_cards()
 
 }
 
-void compare_to_known_images(const Mat& image) 
+//TODO: only call this function when the number of detected cards change or 
+// if there is a significant change between two video frames to avoid unnecessary resource-heavy calculations
+bool compare_to_known_images(const Mat& image) 
 {
-	// load the reference images - do only on the first time
-
+//	double psnrV;
+	Scalar max_mssimV = Scalar(0,0,0);
+//	int psnrTriggerValue = 25;
 	// compare to every reference image
 	for (const auto& ref : card_references) 
 	{
@@ -96,12 +160,37 @@ void compare_to_known_images(const Mat& image)
 		// check overlap
 
 		// return some value if it matches a known reference card good enough
+		
+		/* Adapted from OpenCV tutorial */
+
+		//psnrV = getPSNR(ref, image);
+		//cout << i << ": " << setiosflags(ios::fixed) << setprecision(3) << psnrV << "dB";
+
+		//if (psnrV < psnrTriggerValue && psnrV)
+		//{
+		Scalar mssimV = getMSSIM(ref, image);
+		//}
+		cout << endl;
+
+		if (mssimV[0] > max_mssimV[0] && mssimV[1] > max_mssimV[1] && mssimV[2] > max_mssimV[2]) 
+		{
+			max_mssimV = mssimV;
+		}
 	}
 	// return index of the image with largest overlap, if above a certain threshold
 	// or return the string describing it or sth, no need for OO for this prototype
+	cout << " MSSIM: "
+		<< " R " << setiosflags(ios::fixed) << setprecision(2) << max_mssimV.val[2] * 100 << "%"
+		<< " G " << setiosflags(ios::fixed) << setprecision(2) << max_mssimV.val[1] * 100 << "%"
+		<< " B " << setiosflags(ios::fixed) << setprecision(2) << max_mssimV.val[0] * 100 << "%";
 
+	if (max_mssimV[0] > 0.5 && max_mssimV[1] > 0.5 && max_mssimV[2] > 0.5)
+		return true;
+	else
+		return false;
 }
 
+// TODO: zip with file name or description to later print card name
 void setup_reference_images()
 {
 	// load all the reference images
@@ -142,7 +231,7 @@ int main(int argc, char** argv)
 	/*	---------		CHANGE deviceID to 0 for default front camera! my laptop has a second one on the back i use!	--------- */
 
 	int deviceID = 1;             // 0 = open default camera
-	int apiID = CAP_ANY;      // 0 = autodetect default API
+	int apiID = CAP_MSMF;      // 0 = autodetect default API
 	// open selected camera using selected API
 	cap.open(deviceID + apiID);
 	// check if we succeeded
@@ -150,6 +239,11 @@ int main(int argc, char** argv)
 		cerr << "ERROR! Unable to open camera\n";
 		return -1;
 	}
+	cap.set(CAP_PROP_FRAME_WIDTH, 1280);
+	cap.set(CAP_PROP_FRAME_HEIGHT, 720);
+	double w = cap.get(CAP_PROP_FRAME_WIDTH);
+	double h = cap.get(CAP_PROP_FRAME_HEIGHT);
+	cout << "VideoCapture Resolution: " << w << "x" << h << "\n";
 
 	setup_reference_images();
 
